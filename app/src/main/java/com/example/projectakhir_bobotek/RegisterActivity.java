3dfcfc820a32327dab9fbc781d0fa7758283dbf0
fauzanakbar2503 <107809786+fauzanakbar2503@users.noreply.github.com>
2023-06-15 18:36:56 +0700

    upload image profile (xml & java)

--- app/src/main/java/com/example/projectakhir_bobotek/RegisterActivity.java
+++ app/src/main/java/com/example/projectakhir_bobotek/RegisterActivity.java
@@ -1,14 +1,25 @@
 package com.example.projectakhir_bobotek;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    Button registerBtRegister;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        registerBtRegister=findViewById(R.id.registerBtRegister);
+        registerBtRegister.setOnClickListener(view -> {
+            Intent intent=new Intent(RegisterActivity.this,UploadProfileActivity.class);
+            startActivity(intent);
+        });
+
     }
 }
