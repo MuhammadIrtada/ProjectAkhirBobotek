3a1057001234d28228700a3bdd4c55c847faea47
fauzanakbar2503 <107809786+fauzanakbar2503@users.noreply.github.com>
2023-06-18 23:50:55 +0700

    add logout fiture

--- app/src/main/java/com/example/projectakhir_bobotek/ProfileActivity.java
+++ app/src/main/java/com/example/projectakhir_bobotek/ProfileActivity.java
@@ -2,15 +2,18 @@ package com.example.projectakhir_bobotek;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.projectakhir_bobotek.databinding.ActivityMainBinding;
 import com.example.projectakhir_bobotek.databinding.ActivityProfileBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
 
     ActivityProfileBinding binding;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +21,11 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
         binding = ActivityProfileBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        mAuth = FirebaseAuth.getInstance();
+
         binding.profileBtDownload.setOnClickListener(this);
         binding.profileBtUpload.setOnClickListener(this);
+        binding.profileBtLogout.setOnClickListener(this);
         binding.profileBtSave.setOnClickListener(this);
     }
 
@@ -30,8 +36,18 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
                 break;
             case R.id.profileBtUpload:
                 break;
+            case R.id.profileBtLogout:
+                signOut();
+                break;
             case R.id.profileBtSave:
                 break;
         }
+    }
+
+    private void signOut() {
+        mAuth.signOut();
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
