998f38951d0c47406b956707a0ef086272b5428f
fauzanakbar2503 <107809786+fauzanakbar2503@users.noreply.github.com>
2023-06-18 15:24:28 +0700

    profile (xml)

--- app/src/main/java/com/example/projectakhir_bobotek/HomeActivity.java
+++ app/src/main/java/com/example/projectakhir_bobotek/HomeActivity.java
@@ -2,7 +2,9 @@ package com.example.projectakhir_bobotek;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 
 import com.example.projectakhir_bobotek.databinding.ActivityHomeBinding;
@@ -12,10 +14,19 @@ public class HomeActivity extends AppCompatActivity {
 
     ActivityHomeBinding binding;
 
+    ImageView ivProfile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        ivProfile=findViewById(R.id.ivProfile);
+        ivProfile.setOnClickListener(view -> {
+            Intent intent=new Intent(this,ProfileActivity.class);
+            startActivity(intent);
+
+        });
     }
 }
