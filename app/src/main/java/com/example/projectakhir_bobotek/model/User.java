9a499bcf4821aa54abefac6fdf8f932664fb7231
MuhammadIrtada <88333614+MuhammadIrtada@users.noreply.github.com>
2023-06-18 14:01:45 +0700

    add data user to db

---
+++ app/src/main/java/com/example/projectakhir_bobotek/model/User.java
@@ -0,0 +1,21 @@
+package com.example.projectakhir_bobotek.model;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
+public class User {
+    public String fullName;
+    public String phoneNumber;
+    public String profileImage;
+    public String address;
+    public int saldo;
+
+    public User(){}
+
+    public User(String fullName, String phoneNumber){
+        this.fullName = fullName;
+        this.phoneNumber = phoneNumber;
+        this.saldo = 0;
+        this.profileImage = "/profile.png";
+    }
+}
