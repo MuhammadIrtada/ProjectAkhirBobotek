89bc4892836d7403f4723fd09926af72481d758c
MuhammadIrtada <88333614+MuhammadIrtada@users.noreply.github.com>
2023-06-19 03:37:06 +0700

    add upload image

--- app/src/main/java/com/example/projectakhir_bobotek/HomeActivity.java
+++ app/src/main/java/com/example/projectakhir_bobotek/HomeActivity.java
@@ -1,21 +1,71 @@
 package com.example.projectakhir_bobotek;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 
+import com.bumptech.glide.Glide;
 import com.example.projectakhir_bobotek.databinding.ActivityHomeBinding;
 import com.example.projectakhir_bobotek.databinding.ActivityUploadProfileImageBinding;
+import com.example.projectakhir_bobotek.model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
     ActivityHomeBinding binding;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("users").child(mAuth.getUid()).child("profile");
+        ValueEventListener userListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                updateUI(user);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(HomeActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+            }
+        };
+        mDatabase.addValueEventListener(userListener);
+        binding.homeIcProfile.setOnClickListener(this);
+    }
+
+    private void updateUI(User user) {
+        System.out.println("Nama: " + user.profileImage);
+        Glide.with(getApplicationContext()).load(user.profileImage).into(binding.homeIcProfile);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.homeIcProfile:
+                Intent intent = new Intent(HomeActivity.this, ProfileActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
