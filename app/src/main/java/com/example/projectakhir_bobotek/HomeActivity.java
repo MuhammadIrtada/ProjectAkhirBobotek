bd07cade1c2f268440b2ff877c316e9a7c0b7307
MuhammadIrtada <88333614+MuhammadIrtada@users.noreply.github.com>
2023-06-22 17:03:08 +0700

    edit address profile

--- app/src/main/java/com/example/projectakhir_bobotek/HomeActivity.java
+++ app/src/main/java/com/example/projectakhir_bobotek/HomeActivity.java
@@ -18,7 +18,6 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -75,7 +74,7 @@ public class HomeActivity extends AppCompatActivity {
 
         // Button maps
         binding.homeIvMaps.setOnClickListener(v -> {
-            Intent intent = new Intent(HomeActivity.this, MapsActivity2.class);
+            Intent intent = new Intent(HomeActivity.this, MapsActivity.class);
             startActivity(intent);
         });
 
